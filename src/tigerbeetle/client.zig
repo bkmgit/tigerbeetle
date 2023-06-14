dc43c0cff0d0c0240f13bfa558f3082bfda6edee
Phil Eaton <phil@eatonphil.com>
2023-06-14 18:57:20 -0400

    Make examples a little nicer

--- src/tigerbeetle/client.zig
+++ src/tigerbeetle/client.zig
@@ -466,9 +466,9 @@ pub fn ClientType(comptime StateMachine: type, comptime MessageBus: type) type {
                     \\  Hit enter after a semicolon to run a command.
                     \\
                     \\Examples:
-                    \\  create_accounts id=1 code=1 ledger=1,
-                    \\                  id=2 code=1 ledger=1;
-                    \\  create_transfers id=1 debit_account_id=1 credit_account_id=2 amount=10 ledger=1 code=1;
+                    \\  create_accounts id=1 code=10 ledger=700,
+                    \\                  id=2 code=10 ledger=700;
+                    \\  create_transfers id=1 debit_account_id=1 credit_account_id=2 amount=10 ledger=700 code=10;
                     \\  lookup_accounts id=1;
                     \\  lookup_accounts id=1, id=2;
                     \\
